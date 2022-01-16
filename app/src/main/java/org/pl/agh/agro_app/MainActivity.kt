3b6ab600329b36f6af8ecb2237d277b1f9e88a89
Athanasioschourlias <thanos.chourlias@gmail.com>
2022-01-16 14:09:49 +0100

    Log-in screen UI done

--- app/src/main/java/org/pl/agh/agro_app/MainActivity.kt
+++ app/src/main/java/org/pl/agh/agro_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.pl.agh.agro_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun logInUser(view: android.view.View) {
+        //check if the user entered all the necessary info(e-mail) and if no username autogenerate one! user-001.....
+        //Check if the user exists, if not create one
+        startActivity(Intent(this, menu_activity::class.java))
+    }
+
+    fun guestUser(view: android.view.View) {
+        startActivity(Intent(this, menu_activity::class.java))
     }
 }
